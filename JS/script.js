f532c87fd075b0514f47a2873c4e03cd966bc002
Elvis <steevenbailon23@gmail.com>
2023-10-27 14:51:35 -0500

    Se establecio lineas de separacion en el js para evitar el enriedo de codigo

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// en este primer apartado se alojara el codigo de Johanna 
+
+
+
+
+
+// En este segundo apartado se alojara el codigo de Yuseydi
+
+
+
+
+// En este tercer apartado se alojara el codigo de Elvis 
+

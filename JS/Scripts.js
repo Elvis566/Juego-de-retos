a8e65a297069405f0c6ef450cfd1893751f51ad1
Elvis <steevenbailon23@gmail.com>
2023-10-29 16:19:38 -0500

    Se completo el funcionamiento del juego en el js

--- JS/Scripts.js
+++ JS/Scripts.js
@@ -1,7 +1,9 @@
 // Declaración e inicialización de variables
 var contador = 1; // Contador utilizado para asignar números únicos a los jugadores
-var mitabla = document.getElementById("tabla"); // Obtener la referencia a la tabla en el HTML mediante su ID
+var mitabla = document.getElementById("miTabla"); // Obtener la referencia a la tabla en el HTML mediante su ID
 var total = document.getElementById("jugadores");
+var data = []
+
 
 // Función para guardar la información del jugador
 function guardar(){
@@ -19,7 +21,12 @@ function guardar(){
     // Insertar celda para el nombre de usuario ingresado
     var celda1 = filas.insertCell(1);
     celda1.textContent = document.getElementById("usuario").value;
+
+    // Guardando los nombres e los jugadores en un array
+    data[contador-2] = celda1.textContent;
+    
   }
+  
   function limpiarInputs() {
     // Limpiar el valor de los campos de usuario, jugadores y eli
     document.getElementById("usuario").value = "";
@@ -37,13 +44,16 @@ function guardar(){
         var celdas = filas[i].getElementsByTagName("td");
         // Verificar si la fila tiene celdas y el ID coincide
         if (celdas.length > 0 && celdas[0].innerHTML === Id_eliminar) {
-            mitabla.deleteRow(i);     
+            mitabla.deleteRow(i);   
+            data.splice(i-1,1)  
+            console.log(data);
             let c= total.innerHTML // Se le aasigna el valor de total a la nueva variable
             c= c-1; //C viene con la cantidad de usuarios pero se le asigna un decremento de -1 por cada click en el boton
             total.innerHTML = c; 
             break; // Se encontró y eliminó la fila, salir del bucle
         }
     }
+    
   }
 
   var boton = document.getElementById("b2");
@@ -56,5 +66,8 @@ function guardar(){
   });
   boton2.addEventListener("click", function() {
     // Redirecciona a otra página
-    window.location.href = "../HTML/juego.html";
+    window.location.href = "../HTML/juego.html?datos="+ JSON.stringify(data);;
+    
 });
+
+
--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,21 @@
+// Reciviendo el array que contiene el nombre de los jugadores, que se registraron en la pagina anterior
+var queryString = window.location.search;
+var urlParams = new URLSearchParams(queryString);
+var jugadores = JSON.parse(urlParams.get("datos"));
 
-var baraja= []
+// Declarando el array que contendra las imagenes de las cartas
+var baraja= [];
+
+// Llenando de forma automatica el array con las imagenes
 for(let i=0; i<52; i++){
      baraja[i] =`../img/carta${i}.jpg.jpg`;
 }
-     // console.log(baraja); Verificando que el array se este llenando correctamente
-     // Obtenemos la referencia al elemento de la lista ordenada
+// console.log(baraja); Verificando que el array se este llenando correctamente
+
+// Obtenemos la referencia al elemento de la lista ordenada donde tenemos almecenados los retos
 const listaOrdenada = document.getElementById('miLista');
 
-// Inicializa un array vacío para almacenar los elementos de la lista
+// Inicializa un array vacío para almacenar los elementos de la lista, en este caso los retos
 const elementosArray = [];
 
 // Itera a través de los elementos de la lista y agrega cada elemento al array
@@ -15,30 +23,35 @@ const elementosLista = listaOrdenada.getElementsByTagName('li');
 for (let i = 0; i < elementosLista.length; i++) {
   elementosArray.push(elementosLista[i].textContent);
 }
-
-// Ahora elementosArray contendrá los elementos de la lista como elementos de un array
 // console.log(elementosArray);
-const listaJugadores = document.getElementById('listaJugadores');
 
+// Asignando un avatar a cada jugador ingresado
+var listaJugadores = document.getElementById('listaJugadores');
+for (let i = 0; i < jugadores.length; i++) {
+      // 1. Crea un nuevo elemento de lista (div)
+      var nuevoElemento = document.createElement("div");
+      // 2. Agrega clase css
+      nuevoElemento.className = "e1";
+      // 3. Crea un nodo de texto para el contenido del nuevo elemento
+      var contenido = document.createTextNode(jugadores[i]);
 
-const jugadores = [];
+      // 4. Agrega el nodo de texto al nuevo elemento
+      nuevoElemento.appendChild(contenido);
 
-
-const jugadoresListados = listaJugadores.getElementsByTagName('div');
-for (let i = 0; i < jugadoresListados.length; i++) {
-     jugadores.push(jugadoresListados[i].textContent);
+      // 5. Agrega el nuevo elemento a la lista
+      listaJugadores.appendChild(nuevoElemento);
 }
 
-console.log(jugadores)
+// Referenciando las variable para iterar los retos y su respectiva carta
 var carta = document.getElementById("carta");
 var reto = document.getElementById("reto");
 
 // console.log( Math.floor(Math.random() * 52))
-let c=0;
-let rondasJugadas = [];
-let contadorP = 0;
+let c=0; //contador para iterar y dar funcionamiento
+let rondasJugadas = [];//Array para definir un contador a cada jugador pontador para rondas por jugador
+let contadorP = 0; //contador para contavilizar el numero de partidas
 for(let i=0; i<jugadores.length; i++){
-     rondasJugadas[i]=0;
+     rondasJugadas[i]=0;//iniciando los contadores por ronda de jugador en 0
 }
 function juego(){
      if(c>=jugadores.length){
@@ -60,5 +73,14 @@ function finalizar(){
      for(let i = 0 ; i<jugadores.length ; i++){
           alert(`${jugadores[i]} jugó ${rondasJugadas[i]} veces`);
      }
+
 }
+
+const finalizarBoton = document.getElementById("finalizar");
+
+// Agrega un evento de clic al botón
+finalizarBoton.addEventListener("click", function() {
+    // Redirecciona a otra página
+    window.location.href = "../HTML/usuarios.html";
+});
 

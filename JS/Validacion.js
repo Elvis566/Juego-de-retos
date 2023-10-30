e3f5add0308c007fb321cba4282d4b947b21f077
Elvis <steevenbailon23@gmail.com>
2023-10-30 10:39:45 -0500

    se redirecciono el inicio denseccion

--- JS/Validacion.js
+++ JS/Validacion.js
@@ -9,6 +9,13 @@ if(email==correo)
     if(password==contraseña)
     {
         errorMessage.innerHTML="INGRESO EXITOSO"
+        const Boton = document.getElementById("miBoton");
+
+// Agrega un evento de clic al botón
+    Boton.addEventListener("click", function() {
+    // Redirecciona a otra página
+    window.location.href = "../HTML/usuarios.html";
+});
     }
 }else{
     errorMessage.innerHTML="USUARIO O CONTRASEÑA INCORRECTOS"
